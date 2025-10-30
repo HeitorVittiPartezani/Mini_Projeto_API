c5902fce0de190209e921d4f03dd3d0b0e9787cf
HeitorVittiPartezani <heitorhvp@outlook.com>
2025-10-30 11:14:54 -0300

    Criando endpoint raiz

--- Server/main.py
+++ Server/main.py
@@ -1,3 +1,12 @@
 from fastapi import FastAPI
 from DadosJogos import db_Jogos
 app = FastAPI()
+@app.get("/")
+def raiz():
+    dictraiz = {
+  "mensagem": "Bem-vindo(a) à API de Jogos! 👋",
+  "versao": "1.0",
+  "descricao": "Use o endpoint /jogos para ver a lista de jogos."
+}
+    return dictraiz
+
